0d32bfc2d25bdec12150bd45d865efc7500a206f
F734 <f734@ktgh.com.tw>
2022-11-22 10:11:21 +0800

    add utils/docker-install.sh

---
+++ utils/docker-install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Uninstall old versions
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+# Set up the repository
+# Update the apt package index and install packages to allow apt to use a repository over HTTPS
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# Add Docker’s official GPG key
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+# set up the repository
+echo \
+	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+	$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker Engine
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+# 把當前使用者加到 docker 群組
+sudo usermod -aG docker ${USER}
+
+# Verify that the Docker Engine installation is successful by running the hello-world image
+sudo docker run hello-world
