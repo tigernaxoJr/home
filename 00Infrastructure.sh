247b2b1b0267e9d7139711f3725dd46054d939ba
tigernaxo <tigernaxo@gmail.com>
2024-01-08 17:07:30 +0800

    refactor

--- 00Infrastructure.sh
+++ 00Infrastructure.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 chmod a+x *.sh
-docker network create mynetwork --driver bridge --subnet=172.22.0.0/16
+#docker network create mynetwork --driver bridge --subnet=172.22.0.0/16
+docker network create --driver overlay mynetwork --subnet=172.22.0.0/16
+#docker network create --driver bridge mynetwork
--- 10services.sh
+++ 10services.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-COMMAND=${@:-"up -d"}
-echo ${COMMAND}
-#echo docker-compose -f ha/docker-compose.yaml ${COMMAND} 
-eval "docker-compose -f ha/docker-compose.yaml ${COMMAND}"
-eval "docker-compose -f web/docker-compose.yaml ${COMMAND}"
-eval "docker-compose -f cron/docker-compose.yaml ${COMMAND}"
-#docker-compose -f ha/docker-compose.yaml up -d
+#COMMAND=${@:-"up -d"}
+#echo ${COMMAND}
+
+DIR=$(pwd)
+eval "cd ${DIR}/portainer && docker stack deploy -c <(docker-compose config) portainer"
+eval "cd ${DIR}/hass && docker stack deploy -c <(docker-compose config) hass"
+eval "cd ${DIR}/nginx && docker stack deploy -c <(docker-compose config) nginx"
+
+eval "cd ${DIR}/schedule && docker build -t schedule ."
+eval "cd ${DIR}/schedule && docker stack deploy -c <(docker-compose config) schedule"
